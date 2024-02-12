d631f1386ada2d85ffc93a9030a1e058dbdc66bb
Rafael Oliveira Souza <71654681+rafaoliveira22@users.noreply.github.com>
2024-02-12 13:20:05 -0300

    Initial commit

---
+++ src/main/java/br/com/rafaoliveira/certificartion_nlw_expert/CertificartionNlwExpertApplication.java
@@ -0,0 +1,13 @@
+package br.com.rafaoliveira.certificartion_nlw_expert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CertificartionNlwExpertApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CertificartionNlwExpertApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/rafaoliveira/certificartion_nlw_expert/CertificartionNlwExpertApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rafaoliveira.certificartion_nlw_expert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CertificartionNlwExpertApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
